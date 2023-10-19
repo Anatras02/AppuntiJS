a3ec62bf998427805dca28118767a8c4f1d4b0f5
Alessio Balderi <ingress1234ab@gmail.com>
2023-10-19 18:36:11 +0200

    Cambiato comando up

--- start.bat
+++ start.bat
@@ -2,8 +2,7 @@ REM Pulla il repository
 git pull origin main --no-edit
 
 REM Costruisci e avvia i container con Docker Compose
-docker-compose build
-docker-compose up -d
+docker-compose up -d --build
 
 REM Apri il browser alla pagina del Jupyter Notebook
 start http://localhost:8887
